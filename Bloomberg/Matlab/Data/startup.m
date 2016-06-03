57583e9788feba27d3b3516f65637d93c0f7f37d
simonzcaiman <szhang@caimantrading.com>
2016-06-03 01:09:13 -0500

    Create startup.m

---
+++ Bloomberg/Matlab/Data/startup.m
@@ -0,0 +1,2 @@
+%This is a matlab start up script which add java path to your matlab in order to start your bloomberg connection
+javaaddpath('C:\Users\simonz\Downloads\NoAdmin_DesktopAPI_SDK\DAPI\APIv3\JavaAPI\v3.8.8.2\lib\blpapi3.jar')
